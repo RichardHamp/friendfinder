2b8c743292234a4716ef3f0f24ef6b85e0b45b7d
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-01-11 09:23:13 -0700

    toon array completed

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var toons = [
+	{
+		name: "He-Man",
+		photo: "https://cnet1.cbsistatic.com/img/EXELzMTVtTKK4gxg395yF31MaeQ=/1200x675/2019/12/19/a3a6ed4e-5670-4125-b2bb-b19cc686c767/twitter-in-stream-wide-heman-mattel.jpg",
+		scores: [2, 3, 5, 3, 3, 3, 3, 3, 3, 1]
+	},
+	{
+		name: "Papa Smurf",
+		photo: "https://vignette.wikia.nocookie.net/smurfs/images/b/bd/Papa_Smurf123.png/revision/latest/scale-to-width-down/340?cb=20130805130238",
+		scores: [4, 5, 3, 3, 4, 4, 4, 3, 3, 1]
+	},
+	{
+		name: "My Little Ponies",
+		photo: "https://imgix.ranker.com/user_node_img/50093/1001842205/original/some-of-the-ponies_-names-were-inexplicably-salacious-photo-u2?w=650&q=50&fm=pjpg&fit=crop&crop=faces",
+		scores: [5, 4, 3, 4, 5, 4, 5, 2, 2, 1]
+	},
+	{
+		name: "Funshine Bear",
+		photo: "https://i.pinimg.com/originals/08/62/7c/08627cb22942f4f3f69233be4b10d8e1.jpg",
+		scores: [5, 4, 3, 4, 5, 5, 5, 4, 2, 1]
+	},
+	{
+		name: "Cobra Commander",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYbxyP1a_ZyOtksXVaxVdec2_qwT1AXcA1wLDQnva0ujJAj_8u&s",
+        scores: [1, 2, 2, 5, 1, 1, 1, 3, 4, 5]
+    }
+    {
+		name: "Michalangelo",
+		photo: "https://cnet1.cbsistatic.com/img/EXELzMTVtTKK4gxg395yF31MaeQ=/1200x675/2019/12/19/a3a6ed4e-5670-4125-b2bb-b19cc686c767/twitter-in-stream-wide-heman-mattel.jpg",
+		scores: [3, 3, 4, 2, 2, 2, 4, 3, 3, 1]
+	},
+	{
+		name: "Slimer",
+		photo: "https://s2.dmcdn.net/v/CtnuJ1MYIpL58E2CW/x1080",
+		scores: [4, 5, 5, 2, 4, 3, 3, 3, 4, 3]
+	},
+	{
+		name: "Cy-Kill",
+		photo: "https://vignette.wikia.nocookie.net/gobots/images/c/c9/GobotCyKillTV.jpg/revision/latest/scale-to-width-down/340?cb=20170531205737",
+		scores: [1, 4, 1, 2, 2, 2, 1, 2, 1, 5]
+	},
+	{
+		name: "Heathcliff",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEoyNCYvwViKQyvLuudWvPG_zTTbQ1kRZDa6MuTxTHMVuKpT9qdQ&s",
+		scores: [3, 2, 4, 3, 3, 3, 5, 2, 1, 3]
+	},
+	{
+		name: "Sgt. Slaughter",
+		photo: "https://images.squarespace-cdn.com/content/v1/59fc9fc48a02c7bf8723d617/1554315842940-7DCZQDBUYL9EQNTG84PP/ke17ZwdGBToddI8pDm48kIu7ID3Q_rf864yJf87XFIh7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0h8vX1l9k24HMAg-S2AFielfUZEyfzH2cLnl2HLSR7-WVEkGoqQDaWpNaSSFYXNdhA/really+cool+comic+con+rc3show.com+dickey+beer+set.+slaughter+Howell+hartland+MI+comic+con++JEM",
+		scores: [1, 2, 2, 5, 2, 3, 2, 5, 5, 2]
+	}
+];
